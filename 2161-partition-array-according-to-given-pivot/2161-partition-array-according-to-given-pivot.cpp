412d32d724e42b465aa9aec2685603f09eb03dee
Manan Bhateja <70594765+MananBhateja@users.noreply.github.com>
2022-07-03 20:20:29 +0530

    Time: 362 ms (32.25%), Space: 139.7 MB (5.74%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
+    
+    void func(vector<int>&ans,vector<int>&temp){
+        
+        for(auto x:temp){
+            ans.push_back(x);
+        }
+        
+    }
+    
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         
+        vector<int>before;
         
+        vector<int>same;
+        
+        vector<int>after;
         vector<int>ans;
         
-        
         for(auto x:nums){
+            
             if(x < pivot)
-                ans.push_back(x);
-            
+                before.push_back(x);
+            else if(x == pivot)
+                same.push_back(x);
+            else 
+                after.push_back(x);
         }
         
-        for(auto x:nums){
-            if(x ==pivot)
-                ans.push_back(x);
-            
-        }
-        
-        for(auto x:nums){
-            if(x > pivot)
-                ans.push_back(x);
-            
-        }
-        
+        func(ans,before);
+        func(ans,same);
+        func(ans,after);
         
         return ans;
     }
