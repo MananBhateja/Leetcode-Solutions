8ee9f4eb544a77f56fbeb5be4bc540eeaa63ecd8
Manan Bhateja <70594765+MananBhateja@users.noreply.github.com>
2022-07-04 12:25:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (44.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
-    
-    int bottomup(int m, int n){
-        
-         int dp[100][100]={0};
-        
-        for(int i=0;i<m;i++){
-            
-            
-            for(int j=0;j<n;j++){
-                
-                
-                if(i == 0 or j == 0)
-                    dp[i][j]=1;
-                
-                else
-                {
-                    int ans=0;
-                    
-                    //////if(i-1 >= 0)
-                    ans+=dp[i-1][j];
-                   
-                    
-                  // if(j-1 >= 0)
-                    ans+=dp[i][j-1];
-                  
-                    cout<<ans<<endl;
-                    dp[i][j]=ans;
-                }
-            }
-        }
-        for(int l=0;l<m;l++){
-            
-            for(int k=0;k<n;k++){
-                cout<<dp[l][k]<<" ";
-            }
-            cout<<endl;
-        }
-        return dp[m-1][n-1];
-    }
-    
-    
     int uniquePaths(int m, int n) {
         
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+	//return func(m-1,n-1,dp);
+    dp[0][0]=1;
+       for(int i=0;i<m;i++){
+           
+           for(int j=0;j<n;j++){
+               
+               if(i == 0 and j == 0) dp[i][j] =1;
+               
+              else{
+                   int up=0,left=0;
+               
+               if(i > 0) up=dp[i-1][j];
+               
+               if(j > 0) left =dp[i][j-1];
+               
+               dp[i][j]=up+left;
+              }
+               
+           }
+       }
+    return dp[m-1][n-1];
         
-        return bottomup(m,n);
+        
     }
 };
