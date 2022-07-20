44d4d38f54af65ffd48f69aa25f77e890415a4c8
Manan Bhateja <70594765+MananBhateja@users.noreply.github.com>
2022-07-20 12:31:58 +0530

    Time: 8 ms (26.42%), Space: 12 MB (92.73%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,73 +11,76 @@
  */
 class Solution {
 public:
-    int clevel=0;
-    vector<vector<int>>ans;
     
-    vector<int> rev(vector<int>v){
+    vector<int>rev(vector<int>v){
         
-        vector<int>x;
+        for(auto x:v)
+            cout<<x<<" ";
+        cout<<endl;
+        reverse(v.begin(),v.end());
+         for(auto x:v)
+            cout<<x<<" ";
+        cout<<endl;
         
-        for(int i=v.size()-1;i>=0;i--){
-            
-        x.push_back(v[i]);
-            
-        }
-        return x;
-        
-    }
+    return v;}
     
-    
- void func(TreeNode* root){
+    void func(TreeNode * root,int &clevel, vector<vector<int>>&ans){
         
         vector<int>v;
-    
-            queue<TreeNode*>q;
-            q.push(root);
-            q.push(NULL);
+        queue<TreeNode * >q;
+        q.push(root);
+        q.push(NULL);
         
         while(!q.empty()){
             
-            TreeNode* x = q.front();
+            
+            TreeNode * x=q.front();
             q.pop();
+            
+                if(x == NULL){
 
-            if( x== NULL){
-                
-                if(clevel %2 == 0){
-                    ans.push_back(v);
+                    if(clevel %2 == 0)
+                        ans.push_back(v);
+                    else 
+                        ans.push_back(rev(v));
+
+                    clevel++;
+
+                    if(!q.empty())
+                        q.push(NULL);
+
+                    while(!v.empty())
+                        v.pop_back();
                 }
+            
                 else{
-                    ans.push_back(rev(v));
+
+                    v.push_back(x->val);
+
+                    if(x->left)
+                        q.push(x->left);
+
+                    if(x->right)
+                        q.push(x->right);
+
                 }
-                clevel++;
-                if(!q.empty()){
-                    q.push(NULL);
-                }
-                while(!v.empty()){
-                    v.pop_back();
-                }
-            }
-            else{
-                v.push_back(x->val);
-                if(x->left != NULL) 
-                    q.push(x->left);
-                
-                if(x->right != NULL)
-                    q.push(x->right);
-            }
-        }        
-        
-    }
+            
+            
+         }
+    }   
+    
     
     
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         
-        if(root == NULL){
-          return ans;
-        }
+        vector<vector<int>>ans;
+        if(root == NULL) return ans;
         
-        func(root);
+        int clevel=0;
+        
+         func(root,clevel,ans);
         
         return ans;
+        
     }
 };
